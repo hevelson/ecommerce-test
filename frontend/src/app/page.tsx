274ca9d62ce61255b8daf92e7b819eafd131034c
hevelson <hevelson@gmail.com>
2024-09-13 21:39:48 -0300

    feat: started home page layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
+import NavMenu from "@/components/NavMenu";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: 'Cosméticos & Co - Home',
@@ -7,16 +9,47 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="w-full max-w-[1440px] min-h-screen p-0 pb-8 sm:pb-8 mx-auto font-[family-name:var(--font-geist-sans)]">
+      <header className="w-full">
+        <div className="h-10 flex items-center justify-center px-8 bg-[#4E4E4E] text-white">
+          <p className="text-center">
+            Aproveite as nossas oportunidades!!!
+          </p>
+          <button className="justify-self-end">X</button>
+        </div>
+        <div className="flex h-[110px] items-center px-4 sm:px-[100px]">
+          <Link href="/" className="text-4xl font-semibold">
+            <h1>Cosméticos&CO</h1>
+          </Link>
+          <NavMenu />
+          <form action="">
+            <input type="text" name="search" placeholder="O que você está buscando hoje?" />
+          </form>
+          <div>
+            <a href="/cart">Carrinho</a>
+            <a href="/account">Minha conta</a>
+          </div>
+        </div>
+      </header>
+      <main className="w-full flex flex-col items-center sm:items-start">
+        <div className="w-full bg-red-900 relative">
+          <Image
+            className="dark:invert"
+            src="http://localhost:8080/media/images/banners/DESK_RIR_E_se_02_09_24.jpg"
+            alt="Rock in Rio"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <a
+            className="w-40 absolute top-[50%] left-[90px] rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/category/1"
+            rel="noopener noreferrer"
+          >
+            Comprar agora
+          </a>
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -29,21 +62,6 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
